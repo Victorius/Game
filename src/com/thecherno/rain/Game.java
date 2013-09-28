47b913c13df8f1dee32dfed49a607e5254c75da0
Victorius <ricka_elf@mail.ru>
2013-09-28 15:05:24 +0400

    Episode 3
    The Cherno Project Game programming

---
+++ src/com/thecherno/rain/Game.java
@@ -0,0 +1,31 @@
+package com.thecherno.rain;
+
+public class Game implements Runnable{
+	public static int width = 300;
+	public static int height = width/16*9;
+	public static int scale = 3;
+	
+	private Thread thread;
+	private boolean running = false;
+	public synchronized void start(){
+		running= true;
+		thread = new Thread(this,"Display");
+		thread.start();
+	}
+	@Override
+	public void run() {
+		while(running){
+			
+		}
+		
+	}
+	
+	public synchronized void stop(){
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
