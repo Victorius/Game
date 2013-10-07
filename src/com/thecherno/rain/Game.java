915b4ac6b79cd857db9c903b065edb73c10ec212
Victorius <ricka_elf@mail.ru>
2013-10-07 12:06:44 +0400

    Episode 10
    The Cherno Project Game programming

--- src/com/thecherno/rain/Game.java
+++ src/com/thecherno/rain/Game.java
@@ -68,6 +68,7 @@ public class Game extends Canvas implements Runnable{
 			createBufferStrategy(3);
 			return;
 		}
+		screen.clear();
 		screen.render();
 		for(int i=0;i<pixels.length;i++)
 			pixels[i]= screen.pixels[i];
